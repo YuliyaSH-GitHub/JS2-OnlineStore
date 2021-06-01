74e93df880d6f690531cdd0e41e2cb0b6bae1ffe
Yuliya <shishova-yg@yandex.ru>
2021-06-01 14:12:04 +0300

    добавлено выпадающее меню при клике на корзину

---
+++ scripts/basketButton.js
@@ -0,0 +1,26 @@
+class BasketButton {
+    constructor() {
+        this.basketButton = document.querySelector('.b-OnlineStoreHeader__basketButton');
+        this.dropdownMenu = document.querySelector('.b-OnlineStoreHeader__dropdownMenu');
+    }
+    /**
+     * Метод назначает слушателя события кнопке Корзины
+     */
+    addEventListenerButton() {
+        this.basketButton.addEventListener('click', () => {
+            this.dropdownMenu.style.cssText = "display: block"
+        });
+    }
+    /**
+     * Метод назначает слушателя события выпадающему меню
+     */
+    addEventListenerDropdownMenu() {
+        this.dropdownMenu.addEventListener('click', () => {
+            this.dropdownMenu.style.cssText = "display: none"
+        });
+    }
+
+}
+let basketButton = new BasketButton();
+basketButton.addEventListenerButton();
+basketButton.addEventListenerDropdownMenu();
