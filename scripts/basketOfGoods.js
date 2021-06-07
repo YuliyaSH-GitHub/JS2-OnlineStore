b1ef83484a10adeac394d0ef6d6cabb21b80f01a
Yuliya <shishova-yg@yandex.ru>
2021-06-07 19:25:19 +0300

    не удается получить кнопки

--- scripts/basketOfGoods.js
+++ scripts/basketOfGoods.js
@@ -1,20 +1,8 @@
-const APIBasket = "http://js2-onlinestore/"; //файл json хранится на локальном сервере
+// const APIBasket = "http://js2-onlinestore/"; //файл json хранится на локальном сервере
 class BasketOfGoods {
     constructor() {
         this.basketButton = document.querySelector('.b-OnlineStoreHeader__basketButton');
         this.dropdownMenu = document.querySelector('.b-OnlineStoreHeader__dropdownMenu');
-        this.goodsOfBasket = [];
-        this._addInBasket()
-            .then(data => { // после result.json()) получаем объект data
-                this.goodsOfBasket = data;
-                let {
-                    amount,
-                    countGoods,
-                    contents
-                } = this.goodsOfBasket;
-                // console.log(contents);
-                this.renderGoodsInBasket(contents)
-            });
     }
     /**
      * Метод назначает слушателя события кнопке Корзины
@@ -32,33 +20,13 @@ class BasketOfGoods {
             this.dropdownMenu.style.cssText = "display: none"
         });
     }
-    /**
-     * Метод считает суммарную стоимость товаров в корзине
-     */
-    countTotalCost() {
+    // /**
+    //  * Метод считает суммарную стоимость товаров в корзине
+    //  */
+    // countTotalCost() {
 
-    }
-    _addInBasket() {
-        return fetch(`${APIBasket}/getBasket.json`)
-            .then(result => result.json())
-            .catch(error => {
-                console.log(error);
-            })
-    }
-    renderGoodsInBasket(contents) {
-        contents.forEach(item => {
-            let goodsInBasket =
-                `<tr>
-            <th scope="row">${item.id}</th>
-            <td>${item.title}</td>
-            <td>${item.price}</td>
-            <td class="productCount" data-id="${item.id}">1</td>
-            <td><i class="fas fa-trash-alt productRemoveBtn" data-id="${item.id}"></i></td>
-            </tr>`;
-            let tbody = document.querySelector('tbody');
-            tbody.insertAdjacentHTML('beforeend', goodsInBasket);
-        });
-    }
+    // }
+ 
 
 }
 let basketOfGoods = new BasketOfGoods();
